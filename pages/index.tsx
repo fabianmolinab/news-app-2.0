20310a9d0b01a1bfa3109eb292e1eebfd81a9046
Fabian Molina <molinafabian11b@gmail.com>
2022-11-09 16:03:17 -0500

    fix: Config Chakra Ui and React Icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
+import { Container, Text } from '@chakra-ui/react'
+
 export default function Home() {
-  return <h1>Hello word</h1>
+  return (
+    <Container>
+      <Text> Hola como estas</Text>
+      <h1>Hello word</h1>
+    </Container>
+  )
 }
