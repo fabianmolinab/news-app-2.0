7901ad38d2900deee95a156c546da9735bf5bb2b
Fabian Molina <molinafabian11b@gmail.com>
2022-11-16 02:41:24 -0500

    err

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import { Container, Text } from '@chakra-ui/react'
 
 export default function Home() {
+  const fabian = 'fabito'
+  console.log(fabian)
   return (
     <Container>
       <Text> Hola como estas</Text>
